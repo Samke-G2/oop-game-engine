7db5776cf09d427ce6194f71e0864bad4717b637
Samkelwa <samkelwa101@gmail.com>
2025-10-24 16:05:52 +0200

    Completed most of the code for the individual scenes. I coded the map and it's methods. NEXT STEP is to fine tune the scenes and complete the Engine.

--- map.py
+++ map.py
@@ -1,17 +1,35 @@
 """
 map.py
-last modified: 09/09/2025
+last modified: 24/10/2025
 
 The map file will collect all the scenes of the game and map them out into a dictionary for easy access.
 """
+import scenes.py
 
 class Map(object):
     
+    scenes = {
+        "start_room": scenes.Start(),
+        "chest_room": scenes.Chest_room(),
+        "fire_room": scenes.Fire_room(),
+        "death_room": scenes.Death_room(),
+        "python_room": scenes.Python_room(),
+        "spider_room": scenes.Spider_room(),
+        "finished": scenes.Finished
+    }
+    
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
     
     def next_scene(self, scene_name):
-        pass
+        val = Map.scenes.get(scene_name)
+        
+        return val
     
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
+    
+    
+    
+    
+    
--- scenes.py
+++ scenes.py
@@ -1,6 +1,6 @@
 """
 scenes.py
-last modified: 09/09/2025
+last modified: 24/10/2025
 
 The scenes file will contain the scene class and all its objects, these will run each room (or situation) in the game
 and be mapped for easy access.
@@ -11,6 +11,7 @@ class Player(object):
     
     def __init__(self, name):
         self.name = name
+        self.inventory = ["knife", "torch"]
 
 class Scene(object):
     
@@ -69,7 +70,7 @@ class Chest_room(Scene):
             print(f"\nGood job, {self.player.name}, YOU WIN!\n")
         else:
             self.dead.enter("""
-                You trip, fall, and hit your head on hte chest.
+                You trip, fall, and hit your head on the chest.
                 You die almost immediately.
             """)
         
@@ -78,62 +79,149 @@ class Fire_room(Scene):
     
     def __init__(self):
         super(Fire_room, self).__init__()
+        self.dead = Dead()
     
     def enter(self):
-        pass
-        # call that "dead" scene here too
+        self.dead(dedent("""
+            You enter a room with an angry fire demon in it.
+            The demon spots you and burns you to a crisp.
+        """)) 
         
                 
 class Death_room(Scene):
     
     def __init__(self):
         super(Death_room, self).__init__()
+        self.dead = Dead()
 
     def enter(self):
-        pass
+        # pass
+        print(dedent("""
+            You enter a torch-lit room.
+            There are two doors on either side of you.
+            The door to your left is pitch black, with a bone handle.
+            The door to your right is golden, with a diamond-encrusted handle.
+            In front of you is a throne and on the throne sits a cloaked figure.
+        """))
+        print(dedent(f"""
+            \"Hello, {self.player.name}.\", says the hooded man, \"I have been expecting you.\"
+            \"I am Death, and I offer you a choice.\"
+            \"One of these doors leads to your escape, and the other to your demise.\"
+            \"The choice is yours...\"
+
+            What do you do??
+        """))
         
+        choice = input("> ").lower()
+        
+        if "left" in choice:
+            pass
+            #call the next scene - Chest room
+        elif "right" in choice:
+            pass
+            # call the next scene - Fire room
+        else:
+            self.dead("Death gets annoyed at your indecision and takes your soul.")
         
 
 class Python_room(Scene):
     
     def __init__(self):
-        super(Fire_room, self).__init__()
+        super(Python_room, self).__init__()
+        self.dead = Dead()
     
-    def enter(self, player):
+    def enter(self):
         print(dedent("""
             In this room there is a giant python in the process of strangling a man to death.
             There is also an open door to your right.
             The man notices you and croakes, \"I need your help, stranger. If you kill this snake for me, I can lead you to some hidden treasure.\"
-            \n    What do you do?
+                
+            What do you do?
         """))
         
         choice = input("> ").lower()
-    # This bit needs work on the function calls (will call an object in this iteration) will edit it after restructuring some of the earlier code.
+        
         if ("help" in choice) or ("kill" in choice) or ("save" in choice) or ("cut" in choice) or ("stab" in choice) or ("snake" in choice) :
-            pass
-        #     dead(dedent("""
-        # You miss your attack on the snake and it kills you.
-        #     """))
+            self.dead("You miss your attack on the snake and it kills you.")
         elif "door" in choice:
             pass
             # chest_room(key)
         else:
-            pass
-            # dead("    The python kills the man and slithers over to do the same to you.")
+            self.dead("The python kills the man and slithers over to do the same to you.")
 
 class Spider_room(Scene):
     
     def __init__(self):
-        super(Fire_room, self).__init__()
-    
-    def enter(self, player):
-        self.player = player
+        super(Spider_room, self).__init__()
+        self.dead = Dead()
+        
+    def enter(self):
+        print(dedent("""
+            You enter a room with a  giant spider in it.
+            You notice a golden key on a ring around one of its spider's legs.
+            The spider is preoccupied at the moment while you look around.
+
+            To your left is a doorway, there is a bright light eminating from the room beyond.
+
+            What do you do?
+        """))
+        
+        choice = input("> ").lower()
+        
+        if ("spider" in choice) or ("kill" in choice) or ("cut" in choice) or ("stab" in choice) :
+            print(dedent("""
+                You manage to kill the spider before it notices you. 
+                After it has died, you retrieve the key on its leg
+            """))
+
+            self.player.inventory.append("key")
+
+            print("\nNow what do you do?")
+            next = input("> ").lower()
+            
+            if ("back" in next) or ("return" in next):
+                print(dedent("""
+                    You return to the first room, faced with the same choices:
+                    the door with the snake handle,
+                    the door with the cobwebs,
+                    and the one with the skull and crossbones.
+
+                    Which do you choose now?
+                """))
+
+                sec_choice = input("> ").lower()
+
+                if ("snake" in sec_choice):
+                    pass
+                    # python_room(key)
+                elif ("cobwebs" in sec_choice):
+                    pass
+                    # spider_room(key)
+                elif ("skull" in sec_choice):
+                    pass
+                    # death_room(key)
+                else:
+                    dead("You're paralysed by indecision until you die of starvation")
+
+            elif "left" in next:
+                pass
+                # fire_room()
+            else:
+                self.dead("You fall on your knife and die.")
+                
+        elif ("left" in choice) or ("doorway" in choice):
+            pass
+            # fire_room()
+        else:
+            self.dead("The spider notices you and attacks, paralysing and killing you.")
+        
         
 
 class Start(Scene):
     
     def __init__(self):
-        super(Fire_room, self).__init__()
+        super(Start, self).__init__()
+        self.dead = Dead()
 
     def enter(self, player):
         print(dedent("""
@@ -167,6 +255,17 @@ class Start(Scene):
             pass
             # dead("    You're paralysed by indecision until you die of starvation")
         
+        
+class Finished(Scene):
+    
+    def __init__(self):
+        super(Finished, self).__init__()
+        
+    def enter(self):
+        print(f"Congratulations, {self.player.name}, You Won! ")
+        print("Good Job!")
+        
+    return "finished"
 
 
 
